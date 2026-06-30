81a1e10500ae425d62ee61888c2e8a00cb90d36c
Samuel Silva <130860194+jr0samuel@users.noreply.github.com>
2026-06-30 12:57:04 -0300

    ajuste sobraX

--- src/components/SobraX/sobraX.js
+++ src/components/SobraX/sobraX.js
@@ -6,6 +6,6 @@ export function calcularSobraX(sxVvPc, sxQpPc, sxVsPc) {
     let soxVsPc = numberTrouble(sxVsPc);
     if (soxVvPc === null || soxQpPc === null || soxVsPc === null) return "";
     let conta = (soxVvPc / (soxQpPc + 1 + soxVsPc));
-    if (!isFinite(conta) || isNaN(conta) || soxVvPc === "" || soxQpPc === "" || soxVsPc === "") return "";
+    if (!isFinite(conta) || isNaN(conta) || soxVvPc === "" || soxQpPc === "" || soxVsPc === "" || soxVsPc < 1) return "";
     return conta;
 };
